672e719c2f9f3f7c4cf5cb953ec96cacaf682d4b
qb1968 <mark.allison75@gmail.com>
2023-06-14 19:21:03 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -114,7 +114,7 @@ const App = () => {
 
   return (
     <div className='flex flex-col items-center justify-center mt-10 mb-20 space-y-4'>
-      <h1 className='text-4xl'>React Speech Recognition App 🎧</h1>
+      <h1 className='text-4xl'>MySecretary 🎧</h1>
       <h3 className='text-2xl'>
         Powered by{" "}
         <a
